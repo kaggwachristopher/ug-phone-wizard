b0a5a588b656e188006506234adb2f78d96298e6
Christopher Kaggwa <christopher@laboremus.no>
2022-12-03 05:39:10 +0300

    Update modules/validateNumber.js
    Co-authored-by: codacy-production[bot] <61871480+codacy-production[bot]@users.noreply.github.com>

--- modules/validateNumber.js
+++ modules/validateNumber.js
@@ -1,4 +1,4 @@
-const validUgandanNumber = /^(256|0|\+256)[7]{1}[0-9]{8}$/gim;
+const validUgandanNumber = /^(256|0|\+256)[7][0-9]{8}$/gim;
 
 const validateNumber = (ugandanPhoneNumber) => {
     return validUgandanNumber.test(ugandanPhoneNumber.toString());
